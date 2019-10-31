95174441724146f83109df2d0332cdecf8998711
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2019-10-31 15:42:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ class ApplicationController < Sinatra::Base
     @article = Article.find(params[:id])
     @article.update(params[:article])
 
-    redirect to "/articles/#{@article.id}"
+    redirect to "/articles/#{ @article.id }"
   end
 
   delete '/articles/:id' do
