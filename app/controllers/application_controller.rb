f416b8059883f543b645500875abe2a3cf5440d8
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2019-10-31 15:31:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,4 +48,10 @@ class ApplicationController < Sinatra::Base
 
     redirect to "/articles/#{@article.id}"
   end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+
+    redirect to '/articles'
+  end
 end
